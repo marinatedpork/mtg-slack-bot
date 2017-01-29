396aa6164eae35b075377857cb1d19852af90743
Matthew Meyers <m.j.mey3rs@gmail.com>
2017-01-28 20:03:42 -0600

    POC'd multi card chat bot calls

--- index.js
+++ index.js
@@ -1,9 +1,47 @@
-var request = require('request');
-var token = require('./config/secrets')
-var url = 'https://slack.com/api/rtm.start?token='
+var token = require('./config/secrets');
+var SlackBot = require('slackbots');
+var MTGClient = require('mtgsdk').card;
+var MTGCardRenderer = require('mtg-card-renderer');
+var Renderer = MTGCardRenderer.Renderer;
+var Serializer = MTGCardRenderer.Serializer;
 
-var exampleSocket = new WebSocket("ws://www.example.com/socketserver", "protocolOne");
-
-exampleSocket.onmessage = function (event) {
-  console.log(event.data);
+function parseCards(text) {
+  var PATTERN = /\[\[([\s,.'A-Za-z0-9_]+)\]\]/g;
+  return text.match(PATTERN).map(function(o) {
+    return o.replace('[[', '').replace(']]', '');
+  });
 }
+
+var bot = new SlackBot({
+  token: token,
+  name: 'Jace'
+});
+
+bot.on('start', function() {
+  // bot.postTo('testing', 'Jace is online');
+});
+
+bot.on('message', function(data) {
+  console.log('=============================================');
+  console.log(data);
+  if (data.type === 'message' && data.text) {
+    console.log('IS MESSAGE');
+    var matches = parseCards(data.text);
+    console.log('MATCHES', matches);
+    if (matches.length && typeof matches === 'object') {
+      var queries = matches.forEach(function(o) {
+        console.log(1, o);
+        MTGClient.where({ name: '"' + o + '"'}).then(function(cards) {
+        console.log(2, o);
+          var success = Serializer.deserialize(cards, function(result) {
+            console.log('Deserialized:', result);
+            var response = Renderer(result);
+            console.log('Rendered:', response);
+            bot.postTo('testing', response);
+          });
+        });
+      });
+    }
+  }
+  console.log('=============================================');
+});
