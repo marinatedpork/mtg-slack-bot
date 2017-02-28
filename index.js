cbe67f5a82ca69e955c9e996984809540cb3c68b
Matthew Meyers <m.j.mey3rs@gmail.com>
2017-02-27 22:18:54 -0600

    Refactors project layout, pulls out more values to config, isolates open and message.

--- index.js
+++ index.js
@@ -1,40 +1,17 @@
 const co = require('co');
-const token = require('./config/secrets');
+const config = require('./config/secrets');
 const parseCards = require('./util/parse-cards');
+const message = require('./lib/message');
+const close = require('./lib/close');
 const SlackBot = require('slackbots');
-const { Renderer } = require('mtg-card-renderer');
 const { MongoClient } = require('mongodb');
 
-const name = 'Jace';
-const icon_emoji = ':jace:';
-const BOT_ID = 'B3YGGGQ4W';
+const { token, mongoUrl, collectionName, bot: { id, icon, name } } = config;
 
 co(function*() {
-
   const bot = new SlackBot({ token, name });
-  const db = yield MongoClient.connect('mongodb://localhost:27017/mtg');
-  const collection = db.collection('cards');
-
-  const template = ({ multiverseid }) => {
-    return `http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=${multiverseid}&type=card`;
-  }
-
-  const fn = co.wrap(function*(data) {
-    let { channel, ts: thread_ts, type, text, bot_id } = data;
-
-    if (bot_id === BOT_ID) {
-      return;
-    }
-
-    if (type === 'message' && text) {
-      let matches = parseCards(text);
-      if (matches.length) {
-        let cards = yield matches.map( name => collection.findOne({ name }) );
-        let response = cards.map(Renderer).join('\n');
-        bot.postMessage(channel, response, { thread_ts, icon_emoji });
-      }
-    }
-  });
-
-  bot.on('message', fn);
+  const db = yield MongoClient.connect(mongoUrl);
+  const collection = db.collection(collectionName);
+  bot.on('message', message.bind(null, bot, collection, { id, icon }));
+  bot.on('close', close.bind(null, db));
 });
--- util/parse-cards.js
+++ util/parse-cards.js
@@ -1,6 +1,7 @@
+const PATTERN = /\[\[([\s,.'A-Za-z0-9_]+)\]\]/g;
+
 module.exports = (text) => {
-  var PATTERN = /\[\[([\s,.'A-Za-z0-9_]+)\]\]/g;
-  return (text.match(PATTERN) || []).map((o) => {
-    return o.replace('[[', '').replace(']]', '');
-  });
-};
+  return [...new Set((text.match(PATTERN) || []).map((o) => {
+    return o.replace(/[\[+\]+]/g, '');
+  }))];
+}
