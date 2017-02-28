3ba802d2de3cf64cde931cb60ddc7bb59d96c327
Matthew Meyers <m.j.mey3rs@gmail.com>
2017-02-27 22:23:33 -0600

    Adds Sinon

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
+/**
+ * Library dependencies
+ */
+
 const co = require('co');
+const SlackBot = require('slackbots');
+const { MongoClient } = require('mongodb');
+
+/**
+ * Project modules
+ */
+
 const config = require('./config/secrets');
 const parseCards = require('./util/parse-cards');
 const message = require('./lib/message');
 const close = require('./lib/close');
-const SlackBot = require('slackbots');
-const { MongoClient } = require('mongodb');
+
+/**
+ * Configurations
+ */
 
 const { token, mongoUrl, collectionName, bot: { id, icon, name } } = config;
+
+/**
+ * Run the slack bot
+ */
 
 co(function*() {
   const bot = new SlackBot({ token, name });
