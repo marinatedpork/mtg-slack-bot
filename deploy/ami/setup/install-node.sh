4363d1cff16942369cc5d51b44d9c1529b88c36c
Matthew Meyers <m.j.mey3rs@gmail.com>
2017-04-03 20:22:20 -0500

    Fixes the null resource to make it work without hangup

--- deploy/ami/setup/install-node.sh
+++ deploy/ami/setup/install-node.sh
@@ -8,4 +8,3 @@ echo "Installing Node JS 7.x from deb.nodesource.com..."
 
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 sudo apt-get install -y nodejs
-sudo npm install -g forever
--- deploy/ami/setup/run-slackbot.sh
+++ deploy/ami/setup/run-slackbot.sh
@@ -6,16 +6,10 @@ set -e
 
 echo "Running the Slack Bot..."
 
-LOG_DIR=/var/log/slackbot/bot.log
-
-sudo touch /var/log/slackbot/bot.log
 sudo mkdir /var/log/slackbot
-sudo sh -c 'forever start -a -l $LOG_DIR -o $LOG_DIR -e $LOG_DIR /opt/slackbot/index.js > $LOG_DIR 2>&1 &'
-
-# Do I need pseduo here inside the -c string?
-
-# sudo sh -c 'nohup node /opt/slackbot/index.js > /var/log/slackbot/bot.log 2>&1 &'
+sudo touch /var/log/slackbot/bot.log
+sudo sh -c 'nohup node /opt/slackbot/index.js >> /var/log/slackbot/bot.log 2>&1 &'
 
 echo "Bot started..."
 
-sleep 13
+sleep 20
