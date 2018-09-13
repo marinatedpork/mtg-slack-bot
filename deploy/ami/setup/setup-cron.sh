125c2bbe231dfe4f68641f2c023b300f90fb86d3
Matthew Meyers <m.j.mey3rs@gmail.com>
2018-09-12 19:38:25 -0500

    Updates cron timing and put the killnode script in a better spot.

--- deploy/ami/setup/setup-cron.sh
+++ deploy/ami/setup/setup-cron.sh
@@ -3,5 +3,5 @@ set -e
 echo "Setting up cron job..."
 sudo chmod +x ~/ingest-cards.sh
 
-# Every day at 7 PM Central Standard Time
-(sudo echo "0 0 */1 * * sudo /home/ubuntu/ingest-cards.sh") | crontab -
+# Every day at 1 AM Central Standard Time
+(sudo echo "0 6 */1 * * sudo /home/ubuntu/ingest-cards.sh") | crontab -
